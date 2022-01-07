a95549f5cc0e79191ff04f65875f932e68127757
Muhammet Furkan Özen <62333585+MuhammetFurkanOzenn@users.noreply.github.com>
2022-01-07 04:06:56 +0300

    Loop done
    Now, I almost done each operation for table1.
    After all, I'll copy the table1, 1 to 10 tables and refactor the main function.

--- lokanta.c
+++ lokanta.c
@@ -9,9 +9,9 @@ Detayli Hesap Dokumu
 
 -- Masa 1 Hesabi --
 
-Masa Acma : 99.90 + 19.90 (ilk pirinc tutar) tl
+Masa Acma : 99.90  tl
 Masa Tazeleme Sayisi : 19.90*(masaTazelemeSayisi[i]) tl
-Ne Kadar Pirinc Yendi : 20 *(neKadarPirincYendi[i]) tl
+Ne Kadar Pirinc Yendi : 20 *(neKadarPirincYendi[i])*2 + 19.90 (ilk pirinc tutar) tl
 
 Total Fiyat : ...
  
@@ -20,44 +20,143 @@ Total Fiyat : ...
 
 
 pthread_mutex_t lockMasa1;
-int masa1_m1 = 0;
 
-int rndNo = 50;
-int rndList[50];
+int masa1_m1 = 0;
+int masa1_m2 = 0;
+int masa1_m3 = 0;
+int masa1_m4 = 0;
+int masa1_m5 = 0;
+int masa1_m6 = 0;
+int masa1_m7 = 0;
+int masa1_m8 = 0;
+
+int masa1_yenileme = 0;
+
+int rndNo = 500;
+int rndList[500];
 int neKadarPirincYendi[50]; 
 int masaTazelemeSayisi[50]; 
 
-int r, rnd;
+int r, rnd, numOfPhiGroup;
+int masaCounter = 1;
+
 int i = 0;
-int masaPilav = 13;
- 
+int j = 0;
+int breakTheLoop = 0;
+//int masaPilav = 999999;
+int masadaKalanPilav = 20; 
 
 void *masa1(int tid){
 	
-	while(i < masaPilav){
-		if (i < masaPilav){
-				
+	while(j < masadaKalanPilav){ //&& breakTheLoop == 0
+		printf("brakloop %d\n",breakTheLoop);
+		if (j < masadaKalanPilav ){
+			printf("numOfPhiGroupTh : %d \n",numOfPhiGroup);	
 			rnd = rndList[i]%6;				// random number between 1-5		
 			
 			usleep(1000000*rnd);				// sleep for miliseconds
 				
-		pthread_mutex_lock (&lockMasa1);		// THREAD LOCKED
+			pthread_mutex_lock (&lockMasa1);		// THREAD LOCKED
 				
 			r = rndList[i]%2;				// random number between 0-1 means thinkin' or eatin'
 			
-			printf("random number : %d\n", r);
+			if (breakTheLoop == 0){
+				printf("random number : %d tid : %d i : %d masadakiPilav : %d break : %d\n ", r, tid+1, i, masadaKalanPilav, breakTheLoop);
 			
-			if (tid == 0 && r == 1)
-				masa1_m1 = 1;
-			if (masa1_m1 == 1)
-				printf("tid : %d is true \n\n",tid);
 			
-			pthread_mutex_unlock (&lockMasa1);	// THREAD UNLOCKED
-			i++;
+				if (tid == 0 && r == 1){
+					masa1_m1 = 1;
+					masadaKalanPilav--;
+				}
+				if (tid == 1 && r == 1){
+					masa1_m2 = 1;
+					masadaKalanPilav--;
+				}
+				if (tid == 2 && r == 1){
+					masa1_m3 = 1;
+					masadaKalanPilav--;
+				}
+				if (tid == 3 && r == 1){
+					masa1_m4 = 1;
+					masadaKalanPilav--;
+				}
+				if (tid == 4 && r == 1){
+					masa1_m5 = 1;
+					masadaKalanPilav--;
+				}
+				if (tid == 5 && r == 1){
+					masa1_m6 = 1;
+					masadaKalanPilav--;
+				}
+				if (tid == 6 && r == 1){
+					masa1_m7 = 1;
+					masadaKalanPilav--;
+				}
+				if (tid == 7 && r == 1){
+					masa1_m8 = 1;
+					masadaKalanPilav--;
+				}
+					
+				if (masa1_m1 == 1)
+					printf("phi1 is true \n\n");
+				if (masa1_m2 == 1)
+					printf("phi2 is true \n\n");
+				if (masa1_m3 == 1)
+					printf("phi3 is true \n\n");
+				if (masa1_m4 == 1)
+					printf("phi4 is true \n\n");
+				if (masa1_m5 == 1)
+					printf("phi5 is true \n\n");
+				if (masa1_m6 == 1)
+					printf("phi6 is true \n\n");
+				if (masa1_m7 == 1)
+					printf("phi7 is true \n\n");
+				if (masa1_m8 == 1)
+					printf("phi8 is true \n\n");
+			
+				if ( masadaKalanPilav == 0){	// yemek yeme == 19 i == 19
+					if (masa1_m1 == 1 && masa1_m2 == 1 && masa1_m3 == 1 && masa1_m4 == 1 && masa1_m5 == 1 && masa1_m6 == 1 && masa1_m7 == 1 && masa1_m8 == 1){
+						printf("YEMEK BITTII\n");
+						breakTheLoop = 1;
+						printf("-- Masa %d Detayli Hesap Dokumu --\n", masaCounter);
+						printf("Masa Acma : 99.90 tl \n");	
+						printf("Masa Tazeleme Sayisi : %.2f tl \n",(19.90*masa1_yenileme));
+						printf("Pirinc Fiyati : %.2f tl \n",(20*masa1_yenileme*2) + 19.90);
+						printf("Toplam Fiyat : %.2f tl \n",((19.90*masa1_yenileme) + (20*masa1_yenileme*2 + 19.90) + 99.90));
+						
+						//degiskenleri sifirla
+						//counter for each masa 
+						masaCounter++;
+						}
+					else {
+						masadaKalanPilav = 20;	//i=0;
+						masa1_yenileme++;
+						// pilav yineleme ++
+						// masa yineleme ++
+					}
+					/*
+
+					Detayli Hesap Dokumu
+
+					-- Masa 1 Hesabi --
+
+					Masa Acma : 99.90  tl
+					Masa Tazeleme Sayisi : 19.90*(masaTazelemeSayisi[i]) tl
+					Ne Kadar Pirinc Yendi : 20 *(neKadarPirincYendi[i])*2 + 19.90 (ilk pirinc tutar) tl
+
+					Total Fiyat : ...
+					 
+					*/
+					
+					
+				}
+			}
+				pthread_mutex_unlock (&lockMasa1);	// THREAD UNLOCKED
+				i++;
 		}
 		
 	}
-	printf("masa1 m1 : %d \n",masa1_m1 );	
+	
    	return NULL;
 } 
 
@@ -67,6 +166,9 @@ int main (int argc, char **argv){
 	pthread_t tids[threadSayi];
 	void * status;
 	srand(time(NULL));
+	printf("Kac grup filozofun gelecegini giriniz : ");
+	scanf("%d",&numOfPhiGroup);
+	printf("\numOfPhiGroup : %d \n",numOfPhiGroup);
 	for (int i = 0 ; i < rndNo ; i++ ){
 		rndList[i] = (rand() % rndNo);
 	}
